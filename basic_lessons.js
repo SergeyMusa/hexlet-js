1c7db858046a2390dfae7f3744ee8d03bb7b9ef2
SergeyMusa <sir.stv@gmail.com>
2021-02-18 15:27:30 +0300

    18.02.21

---
+++ basic_lessons.js
@@ -0,0 +1,26 @@
+1 write HelloWorld
+console.log('Hello, World! from MuSa');
+
+2 однострочный коммент
+// You know nothing, Jon Snow!
+
+3 Выведите на экран друг за другом три имени: Robert, Stannis, Renly. 
+console.log('Robert'); console.log('Stannis'); console.log('Renly');
+
+4 Выведите на экран число 9780262531962.
+console.log(9780262531962);
+
+5 Выведите на экран What Is Dead May Never Die
+https://repl.it
+console.log("What Is Dead May Never Die");
+
+6
+
+7
+
+8
+
+9
+
+10
+
