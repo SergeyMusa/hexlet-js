d65ecae4abeb128a9e411560fe56552105e2bacd
SergeyMusa <sir.stv@gmail.com>
2021-02-22 19:27:49 +0300

    22.02.21-night

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ console.log('Hello, Hexlet! from MuSa');
 
 //var _ = require('lodash');
 //var _ = require('./node_modules/lodash');
-var _ = require('./lodash');
+//var _ = require('./lodash');
 
-//import _ require('./lodash');
+import _ from 'lodash';
 
 console.log(_.last(['one', 'two']));
+
